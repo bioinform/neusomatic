3bd44b07a5581e587538722cd4e0c49d6dfdceb0
Sahraeian <sahraeis@lb084.ib.rsshpc1.sc1.roche.com>
2018-09-07 21:14:36 -0700

    updated test message

--- test/run_test.sh
+++ test/run_test.sh
@@ -93,12 +93,12 @@ python ../../neusomatic/python/postprocess.py \
 file1=work_standalone/NeuSomatic_standalone.vcf
 file2=../NeuSomatic_standalone.vcf
 
-cmp --silent $file1 $file2 && echo '### SUCCESS: NeuSomatic stand-alone! ###' \
-|| echo '### WARNING: Files test/NeuSomatic_standalone.vcf and test/example/work_standalone/NeuSomatic_standalone.vcf Are Different! ###'
+cmp --silent $file1 $file2 && echo '### NeuSomatic stand-alone: SUCCESS!  ###' \
+|| echo '### NeuSomatic stand-alone FAILED: Files test/NeuSomatic_standalone.vcf and test/example/work_standalone/NeuSomatic_standalone.vcf Are Different! ###'
 
 
 file1=work_ensemble/NeuSomatic_ensemble.vcf
 file2=../NeuSomatic_ensemble.vcf
 
-cmp --silent $file1 $file2 && echo '### SUCCESS: NeuSomatic ensemble! ###' \
-|| echo '### WARNING: Files test/NeuSomatic_ensemble.vcf and test/example/work_ensemble/NeuSomatic_ensemble.vcf Are Different! ###'
+cmp --silent $file1 $file2 && echo '### NeuSomatic ensemble: SUCCESS! ###' \
+|| echo '### NeuSomatic ensemble FAILED: Files test/NeuSomatic_ensemble.vcf and test/example/work_ensemble/NeuSomatic_ensemble.vcf Are Different! ###'
