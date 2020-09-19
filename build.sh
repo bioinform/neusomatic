9f5d876580053824ec0263d76ad7ee20c01a9757
Sahraeian <sahraeis@lb021login.ib.rsshpc1.sc1.science.roche.com>
2020-09-18 21:52:37 -0700

    fix build

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
 
+if [ -d ${DIR}/neusomatic/build ]; then
+	rm -rf ${DIR}/neusomatic/build
+fi
 rm -rf $DIR/third_party/SeqLib/ $DIR/third_party/seqan/
 pushd $DIR/neusomatic
   mkdir build
